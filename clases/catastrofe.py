ea80499a10fa46f135f8af958a4b38746395fcd4
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-22 13:05:16 +0000

    finalizamos el menu de la pelicula

--- clases/catastrofe.py
+++ clases/catastrofe.py
@@ -1,34 +1,36 @@
 class catastrofe():
-
-  def __init__(self, empresa, empleado, ciudad):
-    self.empresa = empresa
+  def __init__(self, edificio, empleado, ciudad):
+    self.edificio = edificio
     self.empleado = empleado
     self.ciudad = ciudad
-  def catastrofe_ocurrida(self):
-        catastrofes = ("{1}, {2}, {3}")
-        return catastrofes.format(self.edificio, self.empleado, self.ciudad)
+
+
+
+
+
 
 class AlDiaSiguiente():
       def ciudades(new_york, los_angeles):
             while True:
                   print("Menú")
-                  print("1. Qué edificio quieres destruir")
-                  print("2. Qué empleado quieres destruir")
-                  print("3. Qué ciudad quieres destruir")
+                  print("1.  Quieres destruir Nueva York ")
+                  print("2.  Quieres destruir Los Ángeles ")
+                  print("3. No quieres destruir nada")
                   print()
                   eleccion=int(input("Elige una opción: "))
                   break
             if eleccion == 1:
-                      print("Destruir A, B o C")
+                  print("-Se ha destruido Nueva York ")
+                  print("-Se han destruido los edificios A y B ")
+                  print("-Se ha destruido la empresa YooHoo! ")
+                  print("-Fin ")
+            elif eleccion == 2:
+              print("-Se ha destruido Los Ángeles ")
+              print("-Se ha destruido el edificio C ")
+              print("-Se ha destruido la empresa YooHoo! ")
+              print("-Fin ")
+            else:
+                  return
 
-
-          
 print(AlDiaSiguiente.ciudades("new_york " , "los_angeles"))
-            
 
-      
-
-=======
-        return catastrofes.format(self.empresa,self.empleado, self.ciudad)
-    
->>>>>>> 4273b32e95527de6cdb370f39e056bd33997f4a8
