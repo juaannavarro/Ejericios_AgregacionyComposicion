8c0ae82894700467bd8b10a7227a9e85ee6fcee5
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-22 11:06:23 +0000

    Terminamos la primera clase

--- clases/catastrofe.py
+++ clases/catastrofe.py
@@ -1,2 +1,6 @@
 class catastrofe():
-  def 
+  def __init__(self, empresa, empleado, ciudad, edificio):
+    self.empresa = empresa
+    self.empleado = empleado
+    self.ciudad = ciudad
+    self.edificio = edificio
