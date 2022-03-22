4273b32e95527de6cdb370f39e056bd33997f4a8
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-22 11:47:46 +0000

    acabamos la clase catastrofe

--- clases/catastrofe.py
+++ clases/catastrofe.py
@@ -4,4 +4,6 @@ class catastrofe():
     self.empleado = empleado
     self.ciudad = ciudad
   def catastrofe_ocurrida(self):
+        catastrofes = ("{1}, {2}, {3}")
+        return catastrofes.format(self.empresa,self.empleado, self.ciudad)
     
