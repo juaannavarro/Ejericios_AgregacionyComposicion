33b761906ec751fae7fb72247433b7f607464c7b
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-22 16:10:41 +0000

    solucionado

--- clases/yingyang.py
+++ clases/yingyang.py
@@ -1,7 +1,7 @@
 class Yin: pass 
 class Yang: 
-    def __del__(self): 
-        print("Yang destruido") 
+        def __del__(self): 
+            print("Yang destruido") 
  
 yin = Yin() 
 yang = Yang() 
@@ -9,6 +9,7 @@ yin.yang = yang
  
 print(yang) 
 print(yang is yin.yang) 
-del(yang) 
+Yang.__del__(yang) 
 print("?") 
-Yang destruido
+
+#Solución: El del hace que el print al que está asociado tenga mayor importancia. Eso hacía que el (yang)que tenia detrás pertenezca a la clase Yang directamente. Poniendo la clase Yang delante hacemos que la interrogación se introduzca en la clase y no se ponga como opción principal.
